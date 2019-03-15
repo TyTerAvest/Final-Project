9bad0087e06ee8c5912814f92ea37ae38da3070a
Ty Ter Avest <02ty.teravest@gulllakecs.org>
2019-03-15 08:16:17 -0400

    Add comments to explain the code

--- Final Project.py
+++ Final Project.py
@@ -5,12 +5,13 @@ Final Project
 Huge shoutout to Bob Nystrom because I used his code for reference
 When ever I was stuck I just checked what he was doing
 '''
+# The code below makes the maze random everytime
 from random import shuffle, randrange
 def make_maze(w = 16, h = 8):
     vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
     ver = [["|  "] * w + ['|'] for _ in range(h)] + [[]]
     hor = [["+--"] * w + ['+'] for _ in range(h + 1)]
-
+# the code below allows the user to their typing cursor to move through the maze
     def walk(x, y):
         vis[y][x] = 1
 
@@ -21,7 +22,7 @@ def make_maze(w = 16, h = 8):
             if xx == x: hor[max(y, yy)][x] = "+  "
             if yy == y: ver[y][max(x, xx)] = "   "
             walk(xx, yy)
-
+# The code below makes the functions join to create the maze
     walk(randrange(w), randrange(h))
     for (a, b) in zip(hor, ver):
         print(''.join(a + ['\n'] + b))
